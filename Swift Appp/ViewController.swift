ac81a318dad06ff37a744cf8ed9ceb0e4ee60041
SAIDAHMAD <Saidahmad@saidahmads-mbp.home>
2016-12-04 20:06:48 -0500

    Initial Commit

---
+++ Swift Appp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift Appp
+//
+//  Created by SAIDAHMAD on 12/4/16.
+//  Copyright © 2016 SAIDAHMAD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
